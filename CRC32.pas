803c14998ed1daf05907ea765d8f1190b5687efa
František Milt <frantisek.milt@gmail.com>
2016-03-01 16:37:39 +0100

    Corrections for working with file paths containing non-ansi characters

--- CRC32.pas
+++ CRC32.pas
@@ -80,7 +80,14 @@ Function CRC32_Hash(const Buffer; Size: TMemSize): TCRC32;
 implementation
 
 uses
-  SysUtils;
+  SysUtils
+  {$IF Defined(FPC) and not Defined(Unicode)}
+  (*
+    If compiler throws error that LazUTF8 unit cannot be found, you have to
+    add LazUtils to required packages (Project > Project Inspector).
+  *)
+  , LazUTF8
+  {$IFEND};
 
 const
 {$IFDEF LargeBuffer}
@@ -343,7 +350,11 @@ Function FileCRC32(const FileName: String): TCRC32;
 var
   FileStream: TFileStream;
 begin
+{$IF Defined(FPC) and not Defined(Unicode)}
+FileStream := TFileStream.Create(UTF8ToSys(FileName), fmOpenRead or fmShareDenyWrite);
+{$ELSE}
 FileStream := TFileStream.Create(FileName, fmOpenRead or fmShareDenyWrite);
+{$IFEND}
 try
   Result := StreamCRC32(FileStream);
 finally
